2948900549f64c9ec52e6b8544e485927c89a7af
mohitsharma9192 <154876072+mohitsharma9192@users.noreply.github.com>
2024-08-12 21:13:42 +0530

    Add files via upload
    html, CSS, JavaScript

---
+++ main.js
@@ -0,0 +1,179 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
+
+/* Menu show */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/* Menu hidden */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}      
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('bg-header') 
+                       : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
+
+/*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+})
+
+sr.reveal(`.home__data, .footer__conrainer, .footer__group`)
+sr.reveal(`.home__img`, {delay: 700, origin: 'bottom'})
+sr.reveal(`.logos__img, .program__card, .pricing__card` , {interval: 100})
+sr.reveal(`.choose__img, .calculate__content`, {origin: 'left'})
+sr.reveal(`.choose__content, .calculate__imag`, {origin: 'right'})
+
+/*=============== CALCULATE JS ===============*/
+const calculateForm = document.getElementById('calculate-form'),
+      calculateCm = document.getElementById('calculate-cm'),
+      calculateKg = document.getElementById('calculate-kg'),
+      calculateMessage = document.getElementById('calculate-message')
+
+const calculateBmi = (e) =>{
+    e.preventDefault()
+
+    // Check if fields have a value 
+    if(calculateCm.value === '' || calculateKg.value === ''){
+        // Add and remove color 
+        calculateMessage.classList.remove('color-green')
+        calculateMessage.classList.add('color-red')
+
+        // Show massage 
+        calculateMessage.textContent = 'Fill in the Height and Weight'
+
+        // Remove message  three seconds
+        setTimeout(() =>{
+            calculateMessage.textContent = ''
+        }, 3000)
+    }else{
+        // BMI formula
+        const cm = calculateCm.value / 100,
+              kg = calculateKg.value,
+              bmi = Math.round(kg / (cm * cm))
+
+        //  Show your health status
+        if(bmi < 18.5){
+            // Add color and display message 
+            calculateMessage.classList.add('color-green')
+            calculateMessage.textContent = `Your BMI is ${bmi} and you are skinny`
+        }else if(bmi < 25){
+            calculateMessage.classList.add('color-green')
+            calculateMessage.textContent = `Your BMI is ${bmi} and you are healthy`
+        }else{
+            calculateMessage.lassList.add('color-green')
+            calculateMessage.textContent = `Your BMI is ${bmi} and you are overweight`    
+        }
+
+        // To clear the input field
+        calculateCm.value = ''
+        calculateKg.value = ''
+
+        // Remove message four seconds
+        setTimeout(() =>{
+            calculateMessage.textContent = ''
+        }, 4000)
+    }
+} 
+
+calculateForm.addEventListener('submit', calculateBmi)
+
+
+/*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form')
+const contactMessage = document.getElementById('contact-message')
+const contactUser = document.getElementById('contact-user')
+
+const sendEmail = (e) =>{
+    e.preventDefault()
+
+    // check if the field has a value
+    if(contactUser.value === ''){
+        // add amd remove color 
+        contactMessage.classList.remove('color-green')
+        contactMessage.classList.add('color-red')
+
+        // show message 
+        setTimeout(() =>{
+            contactMessage.textContent = ''
+        }, 3000)
+    }else{
+        // serviceid - templateid - #form - publickey 
+        emailjs.sendForm('service_kf8jq44', 'template_rplz2z2', '#contact-form', 'UZRC7bnKo84nf0PR6')
+            .then(() =>{
+                // show message and add color 
+                contactMessage.classList.add('color-green')
+                contactMessage.textContent = 'You registered succesfully'
+
+                // Remove message after three seconds 
+                setTimeout (() =>{
+                    contactMessage.textContent = ''
+
+                }, 3000)
+            }, (error) =>{
+                // Mail sending error 
+                alert('OOPS! SOMETHING HAS FAILED...', error)    
+            })
+        // To clear the input field 
+        contactUser.value = ''    
+    }   
+}
+
+contactForm.addEventListener('submit', sendEmail)
